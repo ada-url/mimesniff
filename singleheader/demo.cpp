3d7c521f38aa807d70aa3e173998bd25e50ea6ce
Khafra <maitken033380023@gmail.com>
2023-06-01 12:58:56 -0400

    first steps (#1)
    add apache license
    
    fix build
    
    fix build
    
    update readme
    
    apply suggestions

--- singleheader/demo.cpp
+++ singleheader/demo.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+#include "ada_mimesniff.cpp"
+#include "ada_mimesniff.h"
+
+using namespace std;
+
 int main(int, char *[]) {
-  // TODO
+  ada::mimesniff::parse_mime_type("text/plain");
+
   return EXIT_SUCCESS;
 }

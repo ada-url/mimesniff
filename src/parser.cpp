7d3b263e8fa7719c225a13734ceaf8b4bd54a1a6
Yagiz Nizipli <yagiz@nizipli.com>
2023-06-10 12:14:27 -0400

    feat: add wip parser.cpp

--- src/parser.cpp
+++ src/parser.cpp
@@ -34,15 +34,17 @@ std::optional<mimetype> parse_mime_type(std::string_view input) {
   // 6. Advance position by 1. (This skips past U+002F (/).)
   position++;
 
-  size_t subtype_end_position = input.find_first_of(';');
-
-  if (subtype_end_position == std::string_view::npos) {
-    // A mimetype may not have parameters. EG. "text/plain"
-    subtype_end_position = input.length();
-  } else if (subtype_end_position == 0) {
+  if (position == input.size()) {
     // If the string doesn't have a subtype, early return. This
     // is part of step #9. EG. "text/"
     return std::nullopt;
+  }
+
+  auto subtype_end_position = input.find_first_of(';', position);
+
+  if (subtype_end_position == std::string_view::npos) {
+    // A mimetype may not have parameters. EG. "text/plain"
+    subtype_end_position = input.size();
   }
 
   // 7. Let subtype be the result of collecting a sequence of code
@@ -63,6 +65,52 @@ std::optional<mimetype> parse_mime_type(std::string_view input) {
   auto out = mimetype();
   out.type = type;        // TODO: Convert this to ASCII lowercase
   out.subtype = subtype;  // TODO: Convert this to ASCII lowercase
+
+  position = subtype_end_position;
+
+  // 11. While position is not past the end of input:
+  while (position < input.size()) {
+    // Advance position by 1. (This skips past U+003B (;).)
+    position++;
+
+    // Collect a sequence of code points that are HTTP whitespace from input
+    // given position.
+    while (is_http_whitespace(input[position])) {
+      position++;
+    }
+
+    // Let parameterName be the result of collecting a sequence of code points
+    // that are not U+003B (;) or U+003D (=) from input, given position.
+    size_t parameter_name_ending = input.find_first_of(";=", position);
+    std::string_view parameter_name =
+        input.substr(position, parameter_name_ending);
+    position += parameter_name_ending;
+    // TODO: Set parameterName to parameterName, in ASCII lowercase.
+
+    // If position is not past the end of input, then:
+    if (position == std::string_view::npos) {
+      // If the code point at position within input is U+003B (;), then
+      // continue.
+      if (input[position] == ';') continue;
+
+      // Advance position by 1. (This skips past U+003D (=).)
+      position++;
+    }
+
+    // If position is past the end of input, then break.
+    if (position >= input.size()) {
+      break;
+    }
+
+    // Let parameterValue be null.
+    std::string_view parameter_value{};
+
+    // If the code point at position within input is U+0022 ("), then:
+    if (input[position] == '"') {
+      // Set parameterValue to the result of collecting an HTTP quoted string
+      // from input, given position and true.
+    }
+  }
 
   return out;
 }

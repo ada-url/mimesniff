0c516bcc159a441ca3f5dfed5501fb1668933b1b
Yagiz Nizipli <yagiz@nizipli.com>
2023-06-10 13:26:26 -0400

    refactor: simplify implementation

--- src/parser.cpp
+++ src/parser.cpp
@@ -8,39 +8,34 @@
 namespace ada::mimesniff {
 
 std::optional<mimetype> parse_mime_type(std::string_view input) {
-  // 1. Remove any leading and trailing HTTP whitespace from input.
+  // Remove any leading and trailing HTTP whitespace from input.
   trim_http_whitespace(input);
 
-  // 2. Let position be a position variable for input, initially
-  //    pointing at the start of input.
-  size_t position = input.find_first_of('/');
-
-  // 5. If position is past the end of input, then return failure.
-  // Also implements part of step 4.
-  if (position == std::string_view::npos || position == 0) {
+  auto type_end_position = input.find_first_of('/');
+  if (type_end_position == std::string_view::npos) {
     return std::nullopt;
   }
 
-  // 3. Let type be the result of collecting a sequence of code
-  //    points that are not U+002F (/) from input, given position.
-  std::string_view type = input.substr(0, position);
+  // Let type be the result of collecting a sequence of code points that are not
+  // U+002F (/) from input, given position.
+  std::string_view type = input.substr(0, type_end_position);
 
-  // 4. If type is the empty string or does not solely contain
-  //    HTTP token code points, then return failure.
-  if (!contains_only_http_tokens(type)) {
+  // If type is the empty string or does not solely contain HTTP token code
+  // points, then return failure.
+  if (type.empty() || !contains_only_http_tokens(type)) {
     return std::nullopt;
   }
 
-  // 6. Advance position by 1. (This skips past U+002F (/).)
-  position++;
+  // Remove type from input. (This skips past U+002F (/).)
+  input.remove_prefix(type_end_position + 1);
 
-  if (position == input.size()) {
+  if (input.empty()) {
     // If the string doesn't have a subtype, early return. This
     // is part of step #9. EG. "text/"
     return std::nullopt;
   }
 
-  auto subtype_end_position = input.find_first_of(';', position);
+  auto subtype_end_position = input.find_first_of(';');
 
   if (subtype_end_position == std::string_view::npos) {
     // A mimetype may not have parameters. EG. "text/plain"
@@ -49,7 +44,7 @@ std::optional<mimetype> parse_mime_type(std::string_view input) {
 
   // 7. Let subtype be the result of collecting a sequence of code
   //    points that are not U+003B (;) from input, given position.
-  std::string_view subtype = input.substr(position, subtype_end_position);
+  std::string_view subtype = input.substr(0, subtype_end_position);
 
   // 8. Remove any trailing HTTP whitespace from subtype.
   trim_trailing_http_whitespace(subtype);
@@ -60,13 +55,16 @@ std::optional<mimetype> parse_mime_type(std::string_view input) {
     return std::nullopt;
   }
 
-  // 10. Let mimeType be a new MIME type record whose type is type,
-  //     in ASCII lowercase, and subtype is subtype, in ASCII lowercase.
+  // Remove subtype from input
+  input.remove_prefix(subtype_end_position);
+
+  // Let mimeType be a new MIME type record whose type is type, in ASCII
+  // lowercase, and subtype is subtype, in ASCII lowercase.
   auto out = mimetype();
   out.type = type;        // TODO: Convert this to ASCII lowercase
   out.subtype = subtype;  // TODO: Convert this to ASCII lowercase
 
-  position = subtype_end_position;
+  size_t position{0};
 
   // 11. While position is not past the end of input:
   while (position < input.size()) {

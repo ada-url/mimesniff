cb5cd52257d74afe971b7ec0f3c0168c9ef8641a
Yagiz Nizipli <yagiz@nizipli.com>
2023-06-11 15:16:13 -0400

    feat: finalize implementation

--- include/ada/mimesniff/util.h
+++ include/ada/mimesniff/util.h
@@ -1,6 +1,7 @@
 #ifndef ADA_MIMESNIFF_UTIL_H
 #define ADA_MIMESNIFF_UTIL_H
 
+#include <string>
 #include <string_view>
 
 namespace ada::mimesniff {
@@ -11,7 +12,21 @@ constexpr inline void trim_trailing_http_whitespace(std::string_view& input);
 
 constexpr inline bool is_http_whitespace(const char c);
 
+constexpr static inline bool is_http_token(const char c);
+
 constexpr inline bool contains_only_http_tokens(std::string_view view);
+
+constexpr static inline bool is_http_quoted_string_token(const char c);
+
+/**
+ * @see https://mimesniff.spec.whatwg.org/#http-quoted-string-token-code-point
+ */
+constexpr inline bool contains_only_http_quoted_string_tokens(
+    std::string_view view);
+
+inline std::string collect_http_quoted_string(std::string_view input,
+                                              size_t& position,
+                                              bool extract_value = false);
 
 }  // namespace ada::mimesniff
 #endif

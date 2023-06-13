7cbc18e5ccc87c72ace619ff7c956c2c50dbb0d0
Daniel Lemire <dlemire@lemire.me>
2023-06-13 18:01:46 -0400

    Reformatting.

--- include/ada/mimesniff/mimetype.h
+++ include/ada/mimesniff/mimetype.h
@@ -12,42 +12,42 @@
 namespace ada::mimesniff {
 
 struct key_value final {
-    key_value() = default;
-    key_value(std::string_view key, std::string_view value) noexcept
-        : stored_key{key}, stored_value{value} {}
-    const std::string_view key() const noexcept { return stored_key; }
-    const std::string_view value() const noexcept { return stored_value; }
-  private:
-    std::string stored_key{};
-    std::string stored_value{};   
+  key_value() = default;
+  key_value(std::string_view key, std::string_view value) noexcept
+      : stored_key{key}, stored_value{value} {}
+  const std::string_view key() const noexcept { return stored_key; }
+  const std::string_view value() const noexcept { return stored_value; }
+
+ private:
+  std::string stored_key{};
+  std::string stored_value{};
 };
 
 struct parameter_list final {
-    parameter_list() = default;
+  parameter_list() = default;
 
-    typedef std::vector<key_value>::iterator iterator;
-    typedef std::vector<key_value>::const_iterator const_iterator;
-    iterator begin() noexcept { return stored_parameters.begin(); }
-    iterator end() noexcept { return stored_parameters.end(); }
-    const_iterator begin() const noexcept { return stored_parameters.begin(); }
-    const_iterator end() const noexcept { return stored_parameters.end(); }
-    const_iterator cbegin() const noexcept { return stored_parameters.cbegin(); }
-    const_iterator cend() const noexcept { return stored_parameters.cend(); }
-    bool empty() const noexcept { return stored_parameters.empty(); }
-    bool contains_key(std::string_view key) const noexcept {
-      return std::find_if(stored_parameters.begin(), stored_parameters.end(),
-                          [&key](const key_value &kv) {
-                            return kv.key() == key;
-                          }) != stored_parameters.end();
-    }
-    void insert(key_value && kv) noexcept {
-      stored_parameters.emplace_back(kv);
-    }
-    void insert(const key_value & kv) noexcept {
-      stored_parameters.emplace_back(kv);
-    }
-  private:
-    std::vector<key_value> stored_parameters{};
+  typedef std::vector<key_value>::iterator iterator;
+  typedef std::vector<key_value>::const_iterator const_iterator;
+  iterator begin() noexcept { return stored_parameters.begin(); }
+  iterator end() noexcept { return stored_parameters.end(); }
+  const_iterator begin() const noexcept { return stored_parameters.begin(); }
+  const_iterator end() const noexcept { return stored_parameters.end(); }
+  const_iterator cbegin() const noexcept { return stored_parameters.cbegin(); }
+  const_iterator cend() const noexcept { return stored_parameters.cend(); }
+  bool empty() const noexcept { return stored_parameters.empty(); }
+  bool contains_key(std::string_view key) const noexcept {
+    return std::find_if(stored_parameters.begin(), stored_parameters.end(),
+                        [&key](const key_value &kv) {
+                          return kv.key() == key;
+                        }) != stored_parameters.end();
+  }
+  void insert(key_value &&kv) noexcept { stored_parameters.emplace_back(kv); }
+  void insert(const key_value &kv) noexcept {
+    stored_parameters.emplace_back(kv);
+  }
+
+ private:
+  std::vector<key_value> stored_parameters{};
 };
 
 struct mimetype {
@@ -86,8 +86,8 @@ struct mimetype {
         // Precede each occurrence of U+0022 (") or U+005C (\) in value with
         // U+005C (\).
         base += '\"';
-        for(char c : i.value()) {
-          switch(c) {
+        for (char c : i.value()) {
+          switch (c) {
             case '"':
             case '\\':
               base += '\\';
@@ -100,7 +100,6 @@ struct mimetype {
       } else {
         base += std::string(i.value());
       }
-
     }
     return base;
   }

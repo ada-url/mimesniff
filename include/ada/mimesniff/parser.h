649c5842896e397e9215c7d3b51cb84085177b00
Daniel Lemire <daniel@lemire.me>
2023-06-13 17:20:45 -0400

    Assuming UTF-8.

--- include/ada/mimesniff/parser.h
+++ include/ada/mimesniff/parser.h
@@ -7,6 +7,8 @@
 
 namespace ada::mimesniff {
 
+// The input is a string_view of the form "type/subtype; param1=value1;
+// It is expected to be UTF-8 encoded, which includes ASCII.
 std::optional<mimetype> parse_mime_type(std::string_view input);
 
 }

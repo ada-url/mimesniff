8cc55797903fbf0e4a606beceb47762365fa3335
Yagiz Nizipli <yagiz@nizipli.com>
2023-06-13 18:25:26 -0400

    test: add more tests

--- tests/wpt_tests.cpp
+++ tests/wpt_tests.cpp
@@ -58,3 +58,40 @@ TEST(wpt_tests, generated_mime_types) {
   }
   SUCCEED();
 }
+
+TEST(wpt_tests, mime_types) {
+  ondemand::parser parser;
+
+  ASSERT_TRUE(file_exists(MIME_TYPES_JSON));
+  padded_string json = padded_string::load(MIME_TYPES_JSON);
+  ondemand::document doc = parser.iterate(json);
+  try {
+    for (auto element : doc.get_array()) {
+      if (element.type() == ondemand::json_type::string) {
+        std::cout << "   section: " << element.get_string() << std::endl;
+      } else if (element.type() == ondemand::json_type::object) {
+        ondemand::object object = element.get_object();
+        std::string_view input = object["input"];
+        std::string_view output{};
+        bool has_null_output = object["output"].get(output);
+
+        std::cout << "    input: " << input << std::endl;
+        std::cout << "    output: " << output << std::endl;
+
+        auto out = ada::mimesniff::parse_mime_type(input);
+
+        ASSERT_EQ(out.has_value(), !has_null_output);
+
+        if (!has_null_output) {
+          ASSERT_EQ(out->serialized(), output);
+        }
+      }
+    }
+  } catch (simdjson::simdjson_error &error) {
+    std::cerr << "JSON error: " << error.what() << " near "
+              << doc.current_location() << " in " << MIME_TYPES_JSON
+              << std::endl;
+    FAIL();
+  }
+  SUCCEED();
+}
